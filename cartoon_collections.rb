c674a38e0aa9836c10a33f860cfa4e924f97c803
Sarah Wise <skjwise@hotmail.com>
2019-08-29 16:46:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,9 +3,9 @@ def roll_call_dwarves(array)
   array.each_with_index { |item, index| puts "#{index + 1}: #{item}" }
 end
 
-def summon_captain_planet(arr)
+def summon_captain_planet(array)
   # Your code here
-  arr.map { |word| puts "#{word.capitalize}!" }
+  array.map { |word| puts "#{word.capitalize}!" }
 end
 
 def long_planeteer_calls# code an argument here
