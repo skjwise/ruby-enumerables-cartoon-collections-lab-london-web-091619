b951cc8ce726d12626fd918ca2e3cf57324ae686
Sarah Wise <skjwise@hotmail.com>
2019-08-29 16:47:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(array)
   array.map { |word| p "#{word.capitalize}!" }
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
